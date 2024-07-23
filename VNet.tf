52811578af2b0cbc583c24297a8871c6ac57cd31
Ryan Stout <ryan.stout@bedegaming.com>
2024-07-23 21:21:11 +0100

    Update VNet.tf

--- VNet.tf
+++ VNet.tf
@@ -3,6 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.virtual_network_resource_group
+  depends_on = [var.virtual_network_resource_group]
 }
 
 resource "azurerm_subnet" "mgmt_subnet" {
