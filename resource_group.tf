b3273122678f2d42c976259d69fac893833bef1d
Ryan Stout <ryan.stout@bedegaming.com>
2024-07-29 14:52:43 +0100

    Update resource_group.tf

--- resource_group.tf
+++ resource_group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "projectvpn" {
-  name     = "projectvpn-rg"
-  location = var.location
-}
+#resource "azurerm_resource_group" "projectvpn" {
+#  name     = "projectvpn-rg"
+#  location = var.location
+#}
