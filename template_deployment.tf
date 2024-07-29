443cbca03b1fe269086d3cb21c76f67d7d31bf74
Ryan Stout <ryan.stout@bedegaming.com>
2024-07-29 14:52:08 +0100

    Update template_deployment.tf

--- template_deployment.tf
+++ template_deployment.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group_template_deployment" "projectvpn" {
   name                = "projectvpn-rg"
-  resource_group_name = azurerm_resource_group.projectvpn.name
+  resource_group_name = projectvpn-2
   deployment_mode     = "Complete"
 
   parameters_content = jsonencode(local.parameters)
