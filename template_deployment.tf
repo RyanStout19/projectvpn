7391fbb43ee466645927ef25fd45ae8d4d847cf8
Ryan Stout <ryan.stout@bedegaming.com>
2024-08-01 16:12:12 +0100

    Update template_deployment.tf

--- template_deployment.tf
+++ template_deployment.tf
@@ -172,6 +172,7 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
                   }
                }
             ]
+         "primary": true
          }
       },
       {
