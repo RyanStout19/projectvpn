349a0d2181aa33fde19c3b3d4403a7861a384ab6
Ryan Stout <ryan.stout@bedegaming.com>
2024-08-02 17:00:47 +0100

    Update template_deployment.tf

--- template_deployment.tf
+++ template_deployment.tf
@@ -9,11 +9,18 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
    "contentVersion": "1.0.0.0",
    "parameters": {
-      "vmName": {
+      "vmNamePrefix": {
          "type": "string",
-         "defaultValue": "asa-vm00",
+         "defaultValue": "asa-vm",
          "metadata": {
-            "description": "Name of the ASA Virtual Machine."
+            "description": "Prefix for the ASA Virtual Machine names."
+         }
+      },
+      "mod_count": {
+         "type": "int",
+         "defaultValue": 1,
+         "metadata": {
+            "description": "Number of Virtual Machines to deploy."
          }
       },
       "adminUsername": {
@@ -60,11 +67,11 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
             "description": "The management interface will attach to this subnet"
          }
       },
-      "mgmtSubnetIP": {
+      "mgmtSubnetIPPrefix": {
          "type": "string",
-         "defaultValue": "${var.mgmt_subnet_ip}",
+         "defaultValue": "${var.mgmt_subnet_ip_prefix}",
          "metadata": {
-            "description": "ASAv IP on the mgmt interface (example: 192.168.0.10)"
+            "description": "Prefix for the ASAv IPs on the mgmt interface (example: 192.168.0.)"
          }
       },
       "data1SubnetName": {
@@ -74,11 +81,11 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
             "description": "The ASAv data1 interface will attach to this subnet"
          }
       },
-      "data1SubnetIP": {
+      "data1SubnetIPPrefix": {
          "type": "string",
-         "defaultValue": "${var.data1_subnet_ip}",
+         "defaultValue": "${var.data1_subnet_ip_prefix}",
          "metadata": {
-            "description": "ASAv IP on the data1 interface (example: 192.168.1.10)"
+            "description": "Prefix for the ASAv IPs on the data1 interface (example: 192.168.1.)"
          }
       },
       "data2SubnetName": {
@@ -88,11 +95,11 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
             "description": "The ASAv data2 interface will attach to this subnet"
          }
       },
-      "data2SubnetIP": {
+      "data2SubnetIPPrefix": {
          "type": "string",
-         "defaultValue": "${var.data2_subnet_ip}",
+         "defaultValue": "${var.data2_subnet_ip_prefix}",
          "metadata": {
-            "description": "ASAv IP on the data2 interface (example: 192.168.2.10)"
+            "description": "Prefix for the ASAv IPs on the data2 interface (example: 192.168.2.)"
          }
       },
       "data3SubnetName": {
@@ -102,11 +109,11 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
             "description": "The ASAv data3 interface will attach to this subnet"
          }
       },
-      "data3SubnetIP": {
+      "data3SubnetIPPrefix": {
          "type": "string",
-         "defaultValue": "${var.data3_subnet_ip}",
+         "defaultValue": "${var.data3_subnet_ip_prefix}",
          "metadata": {
-            "description": "ASAv IP on the data3 interface (example: 192.168.3.10)"
+            "description": "Prefix for the ASAv IPs on the data3 interface (example: 192.168.3.)"
          }
       },
       "vmSize": {
@@ -127,27 +134,16 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
          "metadata": {
             "description": "Location (default: resource group location)"
          }
-      },
-      "mod_count": {
-         "type": "int",
-         "defaultValue": "${var.mod_count}",
-         "metadata": {
-            "description": "Number of Virtual Machines to deploy."
-         }
       }
    },
    "variables": {
-      "VNET_ID": "[resourceId(parameters('virtualNetworkResourceGroup'),'Microsoft.Network/virtualNetworks',parameters('virtualNetworkName'))]",
-      "NIC_ID_0": "[resourceId('Microsoft.Network/networkInterfaces', concat(parameters('vmName'),'-nic-0'))]",
-      "NIC_ID_1": "[resourceId('Microsoft.Network/networkInterfaces', concat(parameters('vmName'),'-nic-1'))]",
-      "NIC_ID_2": "[resourceId('Microsoft.Network/networkInterfaces', concat(parameters('vmName'),'-nic-2'))]",
-      "NIC_ID_3": "[resourceId('Microsoft.Network/networkInterfaces', concat(parameters('vmName'),'-nic-3'))]"
+      "VNET_ID": "[resourceId(parameters('virtualNetworkResourceGroup'),'Microsoft.Network/virtualNetworks',parameters('virtualNetworkName'))]"
    },
    "resources": [
       {
          "type": "Microsoft.Network/networkInterfaces",
          "apiVersion": "2021-02-01",
-         "name": "[concat(parameters('vmName'),'-nic-0')]",
+         "name": "[concat(parameters('vmNamePrefix'), copyIndex(), '-nic-0')]",
          "location": "[parameters('location')]",
          "properties": {
             "ipConfigurations": [
@@ -156,19 +152,23 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
                   "properties": {
                      "primary": true,
                      "privateIPAllocationMethod": "Static",
-                     "privateIPAddress": "[parameters('mgmtSubnetIP')]",
+                     "privateIPAddress": "[concat(parameters('mgmtSubnetIPPrefix'), copyIndex(), '.10')]",
                      "subnet": {
                         "id": "[concat(variables('VNET_ID'),'/subnets/',parameters('mgmtSubnetName'))]"
                      }
                   }
                }
             ]
+         },
+         "copy": {
+            "name": "nicCopy",
+            "count": "[parameters('mod_count')]"
          }
       },
       {
          "type": "Microsoft.Network/networkInterfaces",
          "apiVersion": "2021-02-01",
-         "name": "[concat(parameters('vmName'),'-nic-1')]",
+         "name": "[concat(parameters('vmNamePrefix'), copyIndex(), '-nic-1')]",
          "location": "[parameters('location')]",
          "properties": {
             "ipConfigurations": [
@@ -177,19 +177,23 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
                   "properties": {
                      "primary": false,
                      "privateIPAllocationMethod": "Static",
-                     "privateIPAddress": "[parameters('data1SubnetIP')]",
+                     "privateIPAddress": "[concat(parameters('data1SubnetIPPrefix'), copyIndex(), '.10')]",
                      "subnet": {
                         "id": "[concat(variables('VNET_ID'),'/subnets/',parameters('data1SubnetName'))]"
                      }
                   }
                }
             ]
+         },
+         "copy": {
+            "name": "nicCopy",
+            "count": "[parameters('mod_count')]"
          }
       },
       {
          "type": "Microsoft.Network/networkInterfaces",
          "apiVersion": "2021-02-01",
-         "name": "[concat(parameters('vmName'),'-nic-2')]",
+         "name": "[concat(parameters('vmNamePrefix'), copyIndex(), '-nic-2')]",
          "location": "[parameters('location')]",
          "properties": {
             "ipConfigurations": [
@@ -198,19 +202,23 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
                   "properties": {
                      "primary": false,
                      "privateIPAllocationMethod": "Static",
-                     "privateIPAddress": "[parameters('data2SubnetIP')]",
+                     "privateIPAddress": "[concat(parameters('data2SubnetIPPrefix'), copyIndex(), '.10')]",
                      "subnet": {
                         "id": "[concat(variables('VNET_ID'),'/subnets/',parameters('data2SubnetName'))]"
                      }
                   }
                }
             ]
+         },
+         "copy": {
+            "name": "nicCopy",
+            "count": "[parameters('mod_count')]"
          }
       },
       {
          "type": "Microsoft.Network/networkInterfaces",
          "apiVersion": "2021-02-01",
-         "name": "[concat(parameters('vmName'),'-nic-3')]",
+         "name": "[concat(parameters('vmNamePrefix'), copyIndex(), '-nic-3')]",
          "location": "[parameters('location')]",
          "properties": {
             "ipConfigurations": [
@@ -219,26 +227,30 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
                   "properties": {
                      "primary": false,
                      "privateIPAllocationMethod": "Static",
-                     "privateIPAddress": "[parameters('data3SubnetIP')]",
+                     "privateIPAddress": "[concat(parameters('data3SubnetIPPrefix'), copyIndex(), '.10')]",
                      "subnet": {
                         "id": "[concat(variables('VNET_ID'),'/subnets/',parameters('data3SubnetName'))]"
                      }
                   }
                }
             ]
+         },
+         "copy": {
+            "name": "nicCopy",
+            "count": "[parameters('mod_count')]"
          }
       },
       {
          "type": "Microsoft.Compute/virtualMachines",
          "apiVersion": "2021-03-01",
-         "name": "[parameters('vmName')]",
+         "name": "[concat(parameters('vmNamePrefix'), copyIndex())]",
          "location": "[parameters('location')]",
          "dependsOn": [
-            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmName'), '-nic-0'))]",
-            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmName'), '-nic-1'))]",
-            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmName'), '-nic-2'))]",
-            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmName'), '-nic-3'))]"
-         ],
+            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmNamePrefix'), copyIndex(), '-nic-0'))]",
+            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmNamePrefix'), copyIndex(), '-nic-1'))]",
+            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmNamePrefix'), copyIndex(), '-nic-2'))]",
+            "[resourceId(parameters('virtualNetworkResourceGroup'), 'Microsoft.Network/networkInterfaces', concat(parameters('vmNamePrefix'), copyIndex(), '-nic-3'))]"
+          ],
          "plan": {
             "name": "asav-azure-byol",
             "publisher": "cisco",
@@ -257,60 +269,52 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
                }
             },
             "osProfile": {
-               "computerName": "[parameters('vmName')]",
+               "computerName": "[concat(parameters('vmNamePrefix'), copyIndex())]",
                "adminUsername": "[parameters('adminUsername')]",
                "adminPassword": "[parameters('adminPassword')]"
             },
             "networkProfile": {
                "networkInterfaces": [
                   {
-                     "id": "[variables('NIC_ID_0')]",
+                     "id": "[concat('/subscriptions/', subscription().subscriptionId, '/resourceGroups/', resourceGroup().name, '/providers/Microsoft.Network/networkInterfaces/', parameters('vmNamePrefix'), copyIndex(), '-nic-0')]",
                      "properties": {
-                       "primary": true
+                        "primary": true
                      }
-                   },
-                   {
-                     "id": "[variables('NIC_ID_1')]",
+                  },
+                  {
+                     "id": "[concat('/subscriptions/', subscription().subscriptionId, '/resourceGroups/', resourceGroup().name, '/providers/Microsoft.Network/networkInterfaces/', parameters('vmNamePrefix'), copyIndex(), '-nic-1')]",
                      "properties": {
-                       "primary": false
+                        "primary": false
                      }
-                   },
-                   {
-                     "id": "[variables('NIC_ID_2')]",
+                  },
+                  {
+                     "id": "[concat('/subscriptions/', subscription().subscriptionId, '/resourceGroups/', resourceGroup().name, '/providers/Microsoft.Network/networkInterfaces/', parameters('vmNamePrefix'), copyIndex(), '-nic-2')]",
                      "properties": {
-                       "primary": false
+                        "primary": false
                      }
-                   },
-                   {
-                     "id": "[variables('NIC_ID_3')]",
+                  },
+                  {
+                     "id": "[concat('/subscriptions/', subscription().subscriptionId, '/resourceGroups/', resourceGroup().name, '/providers/Microsoft.Network/networkInterfaces/', parameters('vmNamePrefix'), copyIndex(), '-nic-3')]",
                      "properties": {
-                       "primary": false
+                        "primary": false
                      }
-                   }
+                  }
                ]
             }
          },
          "zones": [
-            "[if(equals(parameters('availabilityZone'),0),json('null'),parameters('availabilityZone'))]"
-         ]
+            "[if(equals(parameters('availabilityZone'), 0), json('null'), parameters('availabilityZone'))]"
+         ],
+         "copy": {
+            "name": "vmCopy",
+            "count": "[parameters('mod_count')]"
+         }
       }
    ],
    "outputs": {
-      "mgmtNIC": {
-         "type": "string",
-         "value": "[variables('NIC_ID_0')]"
-      },
-      "data1NIC": {
-         "type": "string",
-         "value": "[variables('NIC_ID_1')]"
-      },
-      "data2NIC": {
-         "type": "string",
-         "value": "[variables('NIC_ID_2')]"
-      },
-      "data3NIC": {
-         "type": "string",
-         "value": "[variables('NIC_ID_3')]"
+      "vmNames": {
+         "type": "array",
+         "value": "[array(range(0, parameters('mod_count')), concat(parameters('vmNamePrefix'), copyIndex()))]"
       }
    }
 }
