ddcc213ded94b23e70dd337e8b9ec6e4b88f9fbf
Ryan Stout <ryan.stout@bedegaming.com>
2024-08-02 17:15:05 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -91,6 +91,6 @@ variable "base_storage_uri" {
 
 variable "mod_count" {
   description = "Number of Virtual Machines to deploy."
-  type        = number
+  type        = integer
   default     = 2
 }
