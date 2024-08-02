38cd9eb8e3edd1eb68118f1825e228a4ab638ee0
Ryan Stout <ryan.stout@bedegaming.com>
2024-08-02 20:13:06 +0100

    Update template_deployment.tf

--- template_deployment.tf
+++ template_deployment.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group_template_deployment" "projectvpn-asa" {
       },
       "mod_count": {
          "type": "int",
-         "defaultValue": 0,
+         "defaultValue": 1,
          "metadata": {
             "description": "Number of Virtual Machines to deploy."
          }
