845f31ccd8e52c5234e2bfb96169937cae13517a
Ryan Stout <ryan.stout@bedegaming.com>
2024-08-07 11:33:09 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,58 +1,58 @@
-locals {
-  parameters = {
+#locals {
+#  parameters = {
  #   "vmNamePrefix" = {
  #     value = var.vm_name
  #   },
-    "adminUsername" = {
-      value = var.admin_username
-    },
-    "adminPassword" = {
-      value = var.admin_password
-    },
-    "availabilityZone" = {
-      value = var.availability_zone
-    },
-    "vmStorageAccount" = {
-      value = var.vm_storage_account
-    },
-    "virtualNetworkResourceGroup" = {
-      value = var.virtual_network_resource_group
-    },
-    "virtualNetworkName" = {
-      value = var.virtual_network_name
-    },
-    "mgmtSubnetName" = {
-      value = var.mgmt_subnet_name
-    },
-    "mgmtSubnetIPPrefix" = {
-      value = var.mgmt_subnet_ip_prefix
-    },
-    "data1SubnetName" = {
-      value = var.data1_subnet_name
-    },
-    "data1SubnetIPPrefix" = {
-      value = var.data1_subnet_ip_prefix
-    },
-    "data2SubnetName" = {
-      value = var.data2_subnet_name
-    },
-    "data2SubnetIPPrefix" = {
-      value = var.data2_subnet_ip_prefix
-    },
-    "data3SubnetName" = {
-      value = var.data3_subnet_name
-    },
-    "data3SubnetIPPrefix" = {
-      value = var.data3_subnet_ip_prefix
-    },
-    "vmSize" = {
-      value = var.vm_size
-    },
-    "location" = {
-      value = var.location
-    },
-    "baseStorageURI" = {
-      value = var.base_storage_uri
-    },
-  }
-}
+ #   "adminUsername" = {
+ #     value = var.admin_username
+ #   },
+ #   "adminPassword" = {
+ #     value = var.admin_password
+ #   },
+ #   "availabilityZone" = {
+ #     value = var.availability_zone
+ #   },
+ #   "vmStorageAccount" = {
+ #     value = var.vm_storage_account
+ #   },
+ #   "virtualNetworkResourceGroup" = {
+ #     value = var.virtual_network_resource_group
+ #   },
+ #   "virtualNetworkName" = {
+ #     value = var.virtual_network_name
+ #   },
+ #   "mgmtSubnetName" = {
+ #     value = var.mgmt_subnet_name
+ #   },
+ #  value = var.mgmt_subnet_ip_prefix
+ #   "mgmtSubnetIPPrefix" = {
+ #   },
+ #   "data1SubnetName" = {
+ #     value = var.data1_subnet_name
+ #   },
+ #   "data1SubnetIPPrefix" = {
+ #     value = var.data1_subnet_ip_prefix
+ #   },
+ #   "data2SubnetName" = {
+ #     value = var.data2_subnet_name
+ #   },
+ #   "data2SubnetIPPrefix" = {
+ #     value = var.data2_subnet_ip_prefix
+ #   },
+ #   "data3SubnetName" = {
+ #     value = var.data3_subnet_name
+ #   },
+ #   "data3SubnetIPPrefix" = {
+ #     value = var.data3_subnet_ip_prefix
+ #   },
+ #   "vmSize" = {
+ #     value = var.vm_size
+ #   },
+ #   "location" = {
+ #     value = var.location
+ #   },
+ #   "baseStorageURI" = {
+ #     value = var.base_storage_uri
+ #   },
+ # }
+#}
