32a743d75fc1fb597139071830cfe12172d4953a
Ryan <ryan.stout@bedegaming.com>
2024-08-13 19:30:55 +0100

    mod count

--- cisco.tf
+++ cisco.tf
@@ -10,7 +10,7 @@ variable "admin_password" {
 variable "ASAvm_count" {
   description = "Number of ASAvm instances to deploy"
   type        = number
-  default     = 0
+  default     = 1
 }
 resource "azurerm_resource_group" "projectvpn_rg" {
   name     = "projectvpn-2"
